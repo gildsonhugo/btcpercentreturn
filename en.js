3afd4e5b988d39bf605a8c8b9507e337d561f7f1
gildsonhugo <gildson.hugo@gmail.com>
2018-01-22 15:56:57 -0300

    first

---
+++ en.js
@@ -0,0 +1,18 @@
+{
+	"c": "Price Now",
+	"t1": "I bought",
+	"t2": "BTC of",
+	"t3": "The unit value of",
+	"t4": "was",
+	"t5": "BTC",
+	"t6": "So my balance of",
+	"t7": "is:",
+	"t8": "My profit estimate is ",
+	"t9": "%",
+	"t10": "So i have to sell my",
+	"t11": "at:",
+	"t12": "(BTC)",
+	"t13": "and my profit on BTC will be: ",
+	"t14": "Font: ",
+	"place": "name of currency"
+}
---
+++ pt-br.js
@@ -0,0 +1,18 @@
+{
+	"c": "Cotação atual",
+	"t1": "Comprei",
+	"t2": "BTC em",
+	"t3": "O valor unitário do",
+	"t4": "era de",
+	"t5": "BTC",
+	"t6": "Então meu saldo em",
+	"t7": "é de:",
+	"t8": "Minha estimativa de lucro é de ",
+	"t9": "%",
+	"t10": "Então terei que vender meus",
+	"t11": "em:",
+	"t12": "(BTC)",
+	"t13": "E meu lucro em BTC será de: ",
+	"t14": "Fonte: ",
+	"place": "Nome da moeda"
+}
